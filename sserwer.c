6ab8ccb6991eb7d874c2ddd7ed392ad6c6bf6d49
Taczi <39707870+Taczi@users.noreply.github.com>
2019-01-25 20:51:53 +0000

    Update sserwer.c

--- sserwer.c
+++ sserwer.c
@@ -12,9 +12,14 @@
 #include <time.h>
 #include <pthread.h>
 
-#define SERVER_PORT 1220
+#define SERVER_PORT 1122
 #define USER_QUEUE_SIZE 8 //user_number
 #define ROOM_QUEUE_SIZE 4 //user_number
+#define MAX_MSG_LEN 4096
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+char buffer[MAX_MSG_LEN];
 
 //struktura zawierajaca dane, ktore zostana przekazane do watku
 struct thread_data_t
@@ -23,55 +28,6 @@ struct thread_data_t
     struct sockaddr_in caddr;
 };
 
-//funkcja opisujaca zachowanie watku - musi przyjmowac argument typu (void *) i zwracac (void *)
-void *ThreadBehavior(void *t_data)
-{
-    //pthread_detach(pthread_self());
-    struct thread_data_t *th_data = (struct thread_data_t*)t_data;
-    
-    MakeUser(th_data);
-    MakeRoom(th_data);
-
-    //dostep do pol struktury: (*th_data).pole
-    //TODO (przy zadaniu 1) klawiatura -> wysylanie albo odbieranie -> wyswietlanie
-
-    close(th_data->cfd);
-    free(t_data);
-    pthread_exit(NULL);
-}
-
-//funkcja obslugujaca polaczenie z nowym klientem
-void handleConnection(int server_socket_descriptor) {
-    //wynik funkcji tworzacej watek
-    int create_result = 0;
-
-    //uchwyt na watek
-    pthread_t thread1;
-
-    //dane, ktore zostana przekazane do watku
-    //TODO dynamiczne utworzenie instancji struktury thread_data_t o nazwie t_data (+ w odpowiednim miejscu zwolnienie pamieci)
-    //TODO wypelnienie pol struktury
-
-    struct thread_data_t *t_data = malloc(sizeof(struct thread_data_t));
-    socklen_t len = (socklen_t) sizeof(t_data->caddr);
-    
-    t_data->cfd = accept(server_socket_descriptor, (struct sockaddr*) &t_data->caddr, &len);
-
-    create_result = pthread_create(&thread1, NULL, ThreadBehavior, t_data);
-    if (create_result){
-       printf("Blad przy probie utworzenia watku, kod bledu: %d\n", create_result);
-       exit(-1);
-    }
-    pthread_detach(thread1);
-}
-
-void SendMessage(int fd, char * buffer)
-//void SendMessage(int receiverFd, char buffer[50])
-{
-    write(fd, buffer, sizeof(&buffer));
-    //write(receiverFd, buffer, sizeof(&buffer));
-}
-
 //struktura gracza
 struct User
 {
@@ -79,18 +35,6 @@ struct User
     int fd;
     int id;
 };
-struct User usersCount[USER_QUEUE_SIZE];
-int usersCreated = 0;
-
-void MakeUser(struct thread_data_t *thread_data)
-{
-    memset(usersCount[usersCreated].ip, 0, 15);
-    memcpy(usersCount[usersCreated].ip, inet_ntoa((struct in_addr) thread_data->caddr.sin_addr), 15);
-    usersCount[usersCreated].fd = thread_data->cfd;
-    usersCreated++;
-    usersCount[usersCreated].id = usersCreated;
-printf("User zrobiony");
-}
 
 //struktura gry
 struct Room
@@ -98,14 +42,33 @@ struct Room
     int turn;
     int roomId;
     int users;
+    int sent;
+    int now;
     struct User ingame[2];
+    struct User lastplayer[2];
 };
+
 struct Room roomsCount[ROOM_QUEUE_SIZE];
 int roomsCreated = 0;
 
-void MakeRoom(struct thread_data_t *thread_data)
+struct User usersCount[USER_QUEUE_SIZE];
+int usersCreated = 0;
+
+void MUser(struct thread_data_t *thread_data)
+{
+    memset(usersCount[usersCreated].ip, 0, 15);
+    memcpy(usersCount[usersCreated].ip, inet_ntoa((struct in_addr) thread_data->caddr.sin_addr), 15);
+    usersCount[usersCreated].fd = thread_data->cfd;
+    usersCreated++;
+    usersCount[usersCreated].id = usersCreated;
+}
+
+
+void MRoom(struct thread_data_t *thread_data)
 {
     int i, j;
+
+    pthread_mutex_lock(&mutex);
 
     for (i = 0; i < ROOM_QUEUE_SIZE; i++)
     {
@@ -117,12 +80,20 @@ void MakeRoom(struct thread_data_t *thread_data)
                     roomsCount[i].ingame[1] = usersCount[j];
 
             roomsCount[i].users++;
-            SendMessage(thread_data->cfd, "s");
-            SendMessage(thread_data->cfd, "c");
+	    roomsCount[i].turn++;
+            pthread_mutex_unlock(&mutex);
+            strcpy(buffer, "w");
+	    //write(thread_data->cfd, "w", 1);
+	    send(thread_data->cfd, buffer, strlen(buffer), 0);
+            //send(thread_data->cfd, "j"); //joint
+            //SendMessage(thread_data->cfd, "w"); //white
+            bzero(&buffer, sizeof buffer);
             return;
         }
+	
         else if (roomsCount[i].users == 0)
         {
+	    pthread_mutex_unlock(&mutex);
             roomsCount[i].roomId = roomsCreated;
             roomsCount[i].users++;
 
@@ -131,15 +102,115 @@ void MakeRoom(struct thread_data_t *thread_data)
                     roomsCount[i].ingame[0] = usersCount[j];
 
             roomsCreated++;
-            SendMessage(thread_data->cfd, "j" );
-            //printf("Room created");
-            SendMessage(thread_data->cfd, "a");
+            pthread_mutex_unlock(&mutex);
+            strcpy(buffer, "b");
+	    //write(thread_data->cfd, "b", 1);
+	    send(thread_data->cfd, buffer, strlen(buffer), 0);
+            //SendMessage(thread_data->cfd, "s"); //join
+            //SendMessage(thread_data->cfd, "b"); //black
+            bzero(&buffer, sizeof buffer);
             return;
         }
     }
     //SendMessage(thread_data->cfd, "n");
     //printf("Brak wolnych pokoi");
 }
+
+int RGame(struct thread_data_t *thread_data){
+    int i, j;
+    
+    for (i = 0; i < ROOM_QUEUE_SIZE; i++)
+    {
+        for (j = 0; j< 2; j++){
+            if (roomsCount[i].ingame[j].fd == thread_data->cfd)
+            {
+                if (roomsCount[i].turn == 0)
+                {
+	  	    bzero(&buffer, sizeof buffer);
+		    while(recv(thread_data->cfd, buffer, MAX_MSG_LEN, 0) > 0)
+		    {
+			send(roomsCount[i].ingame[1].fd, buffer, strlen(buffer), 0);
+			bzero(&buffer, sizeof buffer);
+
+				//if(strcmp(buffer, "q") == 0 || strcmp(buffer,"d") == 0)
+				//{
+				//	roomsCount[i].now = 0;
+				//} 
+		    } 
+                }
+                //else 
+		if (roomsCount[i].turn == 1)
+                {
+	  	    bzero(&buffer, sizeof buffer);
+		    while(recv(thread_data->cfd, buffer, MAX_MSG_LEN, 0) > 0)
+		    {
+			send(roomsCount[i].ingame[0].fd, buffer, strlen(buffer), 0);
+			bzero(&buffer, sizeof buffer);
+
+				//if(strcmp(buffer, "q") == 0 || strcmp(buffer,"d") == 0)
+				//{
+				//	roomsCount[i].now = 0;
+				//}
+		    }
+                }
+         }
+      }
+    }
+
+}
+
+//funkcja opisujaca zachowanie watku - musi przyjmowac argument typu (void *) i zwracac (void *)
+void *ThreadBehavior(void *t_data)
+{
+    //pthread_detach(pthread_self());
+    struct thread_data_t *th_data = (struct thread_data_t*)t_data;
+
+    pthread_mutex_lock(&mutex);
+    MUser(th_data);
+    pthread_mutex_unlock(&mutex);
+
+    MRoom(th_data);
+
+    //while(strcmp(buffer,"e") != 0 || strcmp(buffer,"d") != 0){
+    while(1){
+    RGame(th_data);}
+
+    close(th_data->cfd);
+    free(t_data);
+    pthread_exit(NULL);
+}
+
+
+//funkcja obslugujaca polaczenie z nowym klientem
+void handleConnection(int server_socket_descriptor) {
+
+    //wynik funkcji tworzacej watek
+    int create_result = 0;
+    //uchwyt na watek
+    pthread_t thread1;
+
+    //dane, ktore zostana przekazane do watku
+    struct thread_data_t *t_data = malloc(sizeof(struct thread_data_t));
+    socklen_t len = (socklen_t) sizeof(t_data->caddr);
+
+    t_data->cfd = accept(server_socket_descriptor, (struct sockaddr*) &t_data->caddr, &len);
+
+    create_result = pthread_create(&thread1, NULL, ThreadBehavior, t_data);
+    if (create_result <0 ){
+       printf("Blad przy probie utworzenia watku, kod bledu: %d\n", create_result);
+       exit(-1);
+    }{printf("Watek jest");}
+
+    pthread_detach(thread1);
+}
+
+
+/*void SendMessage(int fd, char * buffer)
+//void SendMessage(int receiverFd, char buffer[50])
+{
+    send(fd, buffer,sizeof(&buffer)), count, 0);
+    //write(receiverFd, buffer, sizeof(&buffer));
+}*/
 
 /*void MakeOpportunity(struct thread_data_t *thread_data){
 int i, j;
@@ -150,14 +221,12 @@ int i, j;
             {
                 if (roomsCount[i].turn == 1)
                 {
-                    SendMessage(roomsCount[i].ingame[1].fd, "b");
-                    SendMessage(roomsCount[i].ingame[0].fd, "c");
+                    SendMessage(roomsCount[i].ingame[1].fd, "a");
                     roomsCount[i].turn = 2;
                 }
                 else if (roomsCount[i].turn == 2)
                 {
-                    SendMessage(roomsCount[i].ingame[0].fd, "a");
-                    SendMessage(roomsCount[i].ingame[1].fd, "c");
+                    SendMessage(roomsCount[i].ingame[0].fd, "b", 1);
                     roomsCount[i].turn = 1;
                 }
             }
@@ -175,8 +244,10 @@ int main(int argc, char* argv[])
 
    //inicjalizacja gniazda serwera
    memset(&roomsCount, 0, sizeof(struct Room));
-    for (int i = 0; i < ROOM_QUEUE_SIZE; i++)
+   /*for (int i = 0; i < ROOM_QUEUE_SIZE; i++){
         roomsCount[i].turn = 1;
+	roomsCount[i].sent = 2;
+	roomsCount[i].now = 1;}*/
    memset(&server_address, 0, sizeof(struct sockaddr));
    server_address.sin_family = PF_INET;
    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -207,14 +278,11 @@ printf("ok");
 
    while(1)
    {
-       //connection_socket_descriptor = accept(server_socket_descriptor, NULL, NULL);
-      // if (connection_socket_descriptor < 0)
-      // {
-      //     fprintf(stderr, "%s: Blad przy probie utworzenia gniazda dla polaczenia.\n", argv[0]);
-      //     exit(1);
-      // }
+       
 
-       handleConnection(server_socket_descriptor);
+
+handleConnection(server_socket_descriptor);
+
    }
 
    close(server_socket_descriptor);
